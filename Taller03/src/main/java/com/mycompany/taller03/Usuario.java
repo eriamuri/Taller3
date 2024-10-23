69ad03cd625098b83fb55549eb91abefd282a47c
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-10-23 14:19:49 -0500

    Clase usuario (Padre)

---
+++ Taller03/src/main/java/com/mycompany/taller03/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03;
+
+/**
+ *
+ * @author cesar
+ */
+public class Usuario {
+    protected String Usuario;
+    protected String contrasenia;
+    protected String nombre;
+    protected String apellido;
+
+    public Usuario(String Usuario, String contrasenia, String nombre, String apellido) {
+        this.Usuario = Usuario;
+        this.contrasenia = contrasenia;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
