9bca46343cbc718a1d99e927656dd0a82e31d017
eriamuri <eriamuri@espol.edu.ec>
2024-10-23 15:00:27 -0500

    profesor

--- Taller03/src/main/java/com/mycompany/taller03/Profesor.java
+++ Taller03/src/main/java/com/mycompany/taller03/Profesor.java
@@ -1,5 +1,24 @@
 package com.mycompany.taller03;
 
-public class Profesor {
-    
+public class Profesor extends Personal_Academico{
+
+
+
+
+
+    public void cargarActividadSumativa(Curso curso, ActividadSumativa ActividadSumativa){
+        /*aqui va codigo */
+    }
+
+    public void calificarActividadSumativa(Curso curso,Estudiante estudiante, ActividadSumativa ActividadSumativa){
+        /*aqui va codigo */
+    }
+
+    public void aceptarInscripcion(Curso curso, String usuarioEstudiante){
+        /*aqui va codigo */
+    }
+
+    public void crearNuevoForo(Foro foro){
+        /*aqui va codigo */
+    }
 }
