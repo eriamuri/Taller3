05040915193b1eba4d16dc412dc80ba91016fefa
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-10-23 14:59:02 -0500

    Mas clases

--- Taller03/src/main/java/com/mycompany/taller03/ActividadSumativa.java
+++ Taller03/src/main/java/com/mycompany/taller03/ActividadSumativa.java
@@ -4,10 +4,75 @@
  */
 package com.mycompany.taller03;
 
+import java.util.Date;
+
 /**
  *
  * @author cesar
  */
-public class ActividadSumativa {
+abstract class ActividadSumativa  {
+    protected String titulo;
+    protected Date fechaEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
+    protected String id;
+
+    public ActividadSumativa(String titulo, Date fechaEntrega, float puntajeMaximo, String contenido, float calificacion, String id) {
+        this.titulo = titulo;
+        this.fechaEntrega = fechaEntrega;
+        this.puntajeMaximo = puntajeMaximo;
+        this.contenido = contenido;
+        this.calificacion = calificacion;
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Date fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public float getPuntajeMaximo() {
+        return puntajeMaximo;
+    }
+
+    public void setPuntajeMaximo(float puntajeMaximo) {
+        this.puntajeMaximo = puntajeMaximo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public float getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(float calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
 }
