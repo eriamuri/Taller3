a7bcbb01ec00b02ca59e30bdfeee53cc4bb34f47
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-10-23 14:48:27 -0500

    Clases ActividadSumativa, Estudiante, Foro, Peronal Academico

---
+++ Taller03/src/main/java/com/mycompany/taller03/Estudiante.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03;
+
+/**
+ *
+ * @author cesar
+ */
+public class Estudiante extends Personal_Academico {
+    
+    public Estudiante(String Usuario, String contrasenia, String nombre, String apellido) {
+        super(Usuario, contrasenia, nombre, apellido);
+    }
+    public void solicitarInscripcion (Curso curso){
+        
+    }
+    public void realizarActividades(Curso curso , ActividadSumantiva actividadSumativa)
+    
+}
---
+++ Taller03/src/main/java/com/mycompany/taller03/Personal_Academico.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller03;
+
+/**
+ *
+ * @author cesar
+ */
+public class Personal_Academico extends Usuario{
+    
+    public Personal_Academico(String Usuario, String contrasenia, String nombre, String apellido) {
+        super(Usuario, contrasenia, nombre, apellido);
+    }
+    public void reportarError(String email, String contenido){
+        
+    }
+    public void comentarEnForo (Foro foro, String comentario){
+        
+    }
+    public void solicitarAsesoramiento(String email, String Contenido){
+        
+    }
+}
